030880e30a41d697c3ff63fb82d717954e6f0dab
MR-X47 <118037900+MR-X47@users.noreply.github.com>
2023-03-28 13:40:50 +0200

    Update specialization.py

--- specialization.py
+++ specialization.py
@@ -5,18 +5,35 @@ class patient:
 
 class specialization:
     def __init__(self, spec_num):
-        self.normal = list()
-        self.urgent = list()
-        self.super_urgent = list()
+        self.normnl_queue = list()
+        self.urgent_queue = list()
+        self.super_urgent_queue = list()
         self.spec_num = spec_num
+        self.normal_queue_head = 0
+        self.urgent_queue_head = 0
+        self.super_urgent_head = 0
 
     def add_patent(self, patient):
-        if len(self.normal)+len(self.urgent)+len(self.super_urgent) < 10:
+        if len(self.normal)+len(self.urgent_queue)+len(self.super_urgent_queue) < 10:
             if self.getStatus == 0:
                 self.normal.append(patient)
+                self.normal_queue_head += 1
             elif self.getStatus == 1:
-                self.urgent.append(patient)
+                self.urgent_queue.append(patient)
+                self.urgent_queue_head += 1
             elif self.getStatus == 2:
-                self.super_urgent.append(patient)
+                self.super_urgent_queue.append(patient)
+                self.super_urgent_head += 1
         else:
             print("the patient can not be added!")
+
+    def remove_patient(self):
+        if patient in self.normal_queue:
+            self.normal_queue[self.normal_queue_head].pop()
+            self.normal_queue_head += 1
+        elif patient in self.urgent_queue:
+            self.urgent_queue[self.normal_queue_head].pop()
+            self.normal_queue_head += 1
+        elif patient in self.super_urgent_queue:
+            self.super_urgent_queue[self.super_urgent_head].pop()
+            self.super_urgent_head += 1
